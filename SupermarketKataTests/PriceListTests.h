44c094c8d164e42b1b5caa018ea447a72db9d870
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-06-05 17:01:17 +0100

    First stab at a macro to generate the test class

--- SupermarketKataTests/PriceListTests.h
+++ SupermarketKataTests/PriceListTests.h
@@ -2,8 +2,59 @@
 #include <cppUnit/TestCase.h>
 #include <cppUnit/extensions/HelperMacros.h>
 
+#define TEST_FIXTUREOLD(CLASSNAME, T1, T2, T3, ...) \
+class CLASSNAME : public CppUnit::TestFixture \
+{\
+	CPPUNIT_TEST_SUITE(CLASSNAME);\
+	CPPUNIT_TEST(T1); \
+	CPPUNIT_TEST(T2); \
+	CPPUNIT_TEST(T3); \
+	CPPUNIT_TEST_SUITE_END(); \
+public: \
+	void T1(); \
+    void T2(); \
+	void T3(); \
+}
+
+TEST_FIXTUREOLD(PriceListTests2, PriceOfTinOfBeansIs40p, PriceOfTinOfSoupIs90p, PriceOfTinOfPeachesIs55p);
+
+
+#define EXPAND( x ) x
+
+#define FOR_EACH_1(what, x, ...) what(x)
+#define FOR_EACH_2(what, x, ...) what(x) EXPAND(FOR_EACH_1(what, __VA_ARGS__))
+#define FOR_EACH_3(what, x, ...) what(x) EXPAND(FOR_EACH_2(what, __VA_ARGS__))
+#define FOR_EACH_4(what, x, ...) what(x) EXPAND(FOR_EACH_3(what, __VA_ARGS__))
+#define FOR_EACH_5(what, x, ...) what(x) EXPAND(FOR_EACH_4(what, __VA_ARGS__))
+#define FOR_EACH_6(what, x, ...) what(x) EXPAND(FOR_EACH_5(what, __VA_ARGS__))
+#define FOR_EACH_7(what, x, ...) what(x) EXPAND(FOR_EACH_6(what, __VA_ARGS__))
+#define FOR_EACH_8(what, x, ...) what(x) EXPAND(FOR_EACH_7(what, __VA_ARGS__))
+
+
+#define GET_MACRO(_1,_2,_3,_4,_5,NAME,...) NAME 
+#define FOR_EACH(action,...) EXPAND(GET_MACRO(__VA_ARGS__, FOR_EACH_5,FOR_EACH_4,FOR_EACH_3,FOR_EACH_2,FOR_EACH_1)(action, __VA_ARGS__))
+
+#define SIMPLE(name) simple(name)
+#define REGISTER_TEST(name) CPPUNIT_TEST(name);
+#define REGISTER_TESTS(...) FOR_EACH(REGISTER_TEST, __VA_ARGS__)
+#define DECLARE_TEST(name) void name();
+#define DECLARE_TESTS(...) FOR_EACH(DECLARE_TEST, __VA_ARGS__)
+
+#define TEST_FIXTURE(name, ...)\
+class name : public CppUnit::TestFixture {\
+	CPPUNIT_TEST_SUITE(name);\
+	REGISTER_TESTS(__VA_ARGS__);\
+	CPPUNIT_TEST_SUITE_END();\
+public:\
+	DECLARE_TESTS(__VA_ARGS__);\
+};
+
+TEST_FIXTURE(MyTest, a, b, c, d, e);
+
 class PriceListTests: public CppUnit::TestFixture
 {
+
+public:
 	CPPUNIT_TEST_SUITE(PriceListTests);
 	CPPUNIT_TEST(PriceOfTinOfBeansIs40p);
 	CPPUNIT_TEST(PriceOfTinOfSoupIs90p);
@@ -11,7 +62,6 @@ class PriceListTests: public CppUnit::TestFixture
 	CPPUNIT_TEST(PriceOfColeslaw);
 	CPPUNIT_TEST_SUITE_END();
 
-public:
 	void PriceOfTinOfBeansIs40p();
 	void PriceOfTinOfSoupIs90p();
 	void PriceOfTinOfPeachesIs55p();
