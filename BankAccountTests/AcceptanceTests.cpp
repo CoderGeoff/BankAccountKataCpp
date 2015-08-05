d5edb29f1fcc4630ebeea7ea1ee004e7e4f7b822
Geoff Bones <geoff.bones@siemens.com>
2015-08-05 18:34:22 +0100

    Add a placeholder for acceptance tests

---
+++ BankAccountTests/AcceptanceTests.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include <memory>
+
+#include <gtest/gtest.h>
+#include "WithdrawTransaction.h"
+#include <BankAccount.h>
+
+TEST(AcceptanceTests, GivenASavingsAccount_WhenIAttemptToGoOverdrawn_TheTransactionFails)
+{
+    
+}
