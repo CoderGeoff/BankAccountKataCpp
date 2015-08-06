2779686193fc3cc253910b4447c1aa6b20a76e61
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-08-06 10:12:54 +0100

    Improve withdraw transaction test

--- BankAccountTests/WithdrawTransactionTests.cpp
+++ BankAccountTests/WithdrawTransactionTests.cpp
@@ -5,17 +5,16 @@
 #include "WithdrawTransaction.h"
 #include <BankAccount.h>
 
-TEST(WithdrawTransactionTests, GivenNoRules_WhenIExecuteAWithdrawTransaction_TheAccountShouldBeDebited)
+TEST(WithdrawTransactionTests, GivenAnAccountWith$10AndNoRules_WhenIWithdraw$6_ThenBalanceShouldBe$4)
 {
     // Given
-    auto initialBalance = 1000;
     std::shared_ptr<BankAccount> account(new BankAccount(CurrentAccount));
-    account->SetBalance(initialBalance);
+    account->SetBalance(1000);
 
     // When
     std::unique_ptr<WithdrawTransaction> withdrawFromAccount(new WithdrawTransaction(account));
     withdrawFromAccount->Execute(600);
 
     // Then
-    ASSERT_LT(account->GetBalance(), initialBalance);
+    ASSERT_EQ(400, account->GetBalance());
 }
